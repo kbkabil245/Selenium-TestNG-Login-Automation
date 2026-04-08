23155232d133a5259e8ad881d9c2c55ecfd0523c
K.B.KABILAN <kabilboopathi245@gmail.com>
2026-04-08 13:40:50 +0530

    Screenshot_Utility.java

---
+++ utils/Screenshot_Utility.java
@@ -0,0 +1,20 @@
+package selenium_project;
+
+import org.openqa.selenium.*;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+public class Screenshot_Utility {
+
+		    public static void takeScreenshot(WebDriver driver, String name) {
+		    	
+		        try {
+		            TakesScreenshot ts = (TakesScreenshot) driver;
+		            File src = ts.getScreenshotAs(OutputType.FILE);
+		            FileUtils.copyFile(src, new File("screenshots/" + name + ".png"));
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		    }
+	}
+
