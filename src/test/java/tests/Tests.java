14d396c53a079707ce5ca56ffdaeeef9fcbeaf1d
K.B.KABILAN <kabilboopathi245@gmail.com>
2026-04-08 17:18:45 +0530

    Tests.java

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -1,28 +1,36 @@
 package selenium_project;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 		public class Tests extends BaseTest  {
 
 		    @Test
 		    public void validLogin() throws Exception {
 		        LoginPage lp = new LoginPage(driver);
-		        lp.login("standard_user","secret_sauce");
+		        lp.login("standard_user","secret_sauce1");
 		        String expected = "Swag Labs";
 		        Thread.sleep(3000);
-		        Screenshot_Utility.takeScreenshot(driver,"Login Success");
 		        String actual = driver.getTitle();
-		        System.out.println(actual);
 		        
 		        boolean isDashboardPresent = driver.findElements(By.id("inventory_container")).size() > 0;
 
-		        if(isDashboardPresent){
-		            System.out.println("Test Case Pass (Login Success)");
-		        } else {
-		            System.out.println("Test Case Fail (Error displayed)");
+		        if (isDashboardPresent) {
+		        	Screenshot_Utility.takeScreenshot(driver, "Login_Success)");
+		        	System.out.println("Test Case Pass(Login Success)");
+		        }else {
+		        	Screenshot_Utility.takeScreenshot(driver,"Login_Failed");
+		        	System.out.println("Test Case Fail(Error displayed)");
+		        }
+		        Assert.assertEquals(actual, expected,"Title Mismatch!");
+		            Assert.assertTrue(isDashboardPresent, "Dashboard not found!");
 		        }
 		    }
-		        }
+		
+		        
 		    
 		
+
 
